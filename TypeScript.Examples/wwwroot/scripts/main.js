14431818419ef1b363d8975010c1b7c5d060eeee
an <ariel_napoli@hotmail.com>
2017-09-12 14:22:54 -0300

    .-

---
+++ TypeScript.Examples/wwwroot/scripts/main.js
@@ -0,0 +1,5 @@
+"use strict";
+exports.__esModule = true;
+var browser_1 = require("angular2/platform/browser");
+var app_1 = require("./app");
+browser_1.bootstrap(app_1.MyApp);
---
+++ TypeScript.Examples/wwwroot/scripts/model.js
@@ -0,0 +1,9 @@
+"use strict";
+exports.__esModule = true;
+var MyModel = /** @class */ (function () {
+    function MyModel() {
+        this.compiler = "TypeScript";
+    }
+    return MyModel;
+}());
+exports.MyModel = MyModel;
